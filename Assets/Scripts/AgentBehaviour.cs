f20ea7fa87195eb9012bd72e7ea1b38a93359945
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-04-04 08:50:07 +0700

    Add Agent Movement

---
+++ Assets/Scripts/AgentBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBehaviour : MonoBehaviour
+{
+
+    public float speed;
+    [Range(0, 2)]
+    public float stopRange;
+    public Transform target;
+
+    private float stopPoint;
+
+    private void Start()
+    {
+        stopPoint = Random.Range(0, stopRange);
+    }
+
+    private void Update()
+    {
+        MoveTo();
+    }
+
+
+    private void MoveTo()
+    {
+        if(Vector3.Distance(transform.position, target.position) > stopPoint)
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
