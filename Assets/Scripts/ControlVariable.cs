1f1180cb169f5227f29015d35e6a5e78a7089363
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-04-04 22:46:11 +0700

    Add ControlVariable for virus infection duration

---
+++ Assets/Scripts/ControlVariable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MainControlVariable", menuName = "Control/MainControlVariable")]
+public class ControlVariable : ScriptableObject
+{
+
+    public float InfectionTime;
+
+    public static ControlVariable Instance
+    {
+        get
+        {
+            return Resources.Load<ControlVariable>("MainControlVariable");
+        }
+    }
+
+}
