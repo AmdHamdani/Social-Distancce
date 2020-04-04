092e1b5c2e77bae2ab5238590fcb9bfe1f15ae6f
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-04-04 22:46:32 +0700

    Add Random Stuff

---
+++ Assets/Scripts/ValueUtil.cs
@@ -0,0 +1,12 @@
+﻿public class ValueUtil
+{
+    public static T DefaultClass<T>() where T : class, new()
+    {
+        return new T();
+    }
+
+    public static T DefaultStruct<T>() where T : struct
+    {
+        return new T();
+    }
+}
