241ec3eda84200cfd3c6a61c8ecbe41373aa247e
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-04-05 21:28:09 +0700

    Add Target Pool

---
+++ Assets/Scripts/TargetPool.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPool : MonoBehaviour
+{
+
+    public Transform CurrentTarget;
+    public List<GameObject> targets = new List<GameObject>();
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        targets.AddRange(GameObject.FindGameObjectsWithTag("Target"));
+        CurrentTarget = targets[Random.Range(0, targets.Count)].transform;
+    }
+
+}
