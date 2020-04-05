287d6de1f08453749a0d31871c364950944a3655
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-04-05 22:14:58 +0700

    Add Game End

--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class InGameUI : MonoBehaviour
@@ -7,13 +8,18 @@ public class InGameUI : MonoBehaviour
 
     public Text timerText;
     public Text infectedCounter;
+    public GameObject gameEnd;
+    public Text timeScore;
+    public Text score;
+    public Button homeButton;
 
     private float interval = 0;
     private float totalPeople = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
+        homeButton.onClick.AddListener(() => SceneManager.LoadScene("Menu"));
+
         totalPeople = GameObject.FindGameObjectsWithTag("Person").Length - 1;
 
         ControlVariable.Ins.TotalInfected = 0;
@@ -40,8 +46,10 @@ public class InGameUI : MonoBehaviour
 
             if ((second <= 0 & interval <= 0) || totalPeople == ControlVariable.Ins.TotalInfected)
             {
-                Debug.Log("Game Over");
+                yield return new WaitForSeconds(1f);
+                ShowGameEnd();
                 StopCoroutine(UpdateTime());
+                break;
             }
             else
             {
@@ -53,4 +61,10 @@ public class InGameUI : MonoBehaviour
         }
     }
 
+    private void ShowGameEnd()
+    {
+        timeScore.text = ControlVariable.Ins.TimerInMinutes * 60 + " s";
+        score.text = (100 - ((ControlVariable.Ins.TotalInfected / totalPeople) * 100)) + "";
+        gameEnd.SetActive(true);
+    }
 }
