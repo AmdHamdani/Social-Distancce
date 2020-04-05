641b2ffba8518b68655477c65bdd4670c33b8bcf
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-04-05 22:15:16 +0700

    Add Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Button startButton;
+    public GameObject instructionOne;
+    public GameObject instructionTwo;
+    public Button nextInstruction;
+    public Button playGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(() => instructionOne.SetActive(true));
+        nextInstruction.onClick.AddListener(() => instructionTwo.SetActive(true));
+        playGame.onClick.AddListener(() => SceneManager.LoadScene("Gameplay"));
+    }
+
+}
