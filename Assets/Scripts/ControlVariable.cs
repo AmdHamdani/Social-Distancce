2be5ff9b11171fab432583583c4b9b90b8053022
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-04-05 21:27:43 +0700

    Add more Control Variable

--- Assets/Scripts/ControlVariable.cs
+++ Assets/Scripts/ControlVariable.cs
@@ -4,7 +4,18 @@
 public class ControlVariable : ScriptableObject
 {
 
-    public float InfectionTime;
+    public float MinInfectionTime;
+    public float MaxInfectionTime;
+    public float TimerInMinutes;
+    public float TotalInfected;
+
+    public float InfectionTime
+    {
+        get
+        {
+            return Random.Range(MinInfectionTime, MaxInfectionTime);
+        }
+    }
 
     public static ControlVariable Instance
     {
